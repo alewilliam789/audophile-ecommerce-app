ef42c4bc4a1384c7bb5a8970e889621a18677896
alewilliam789 <alewilliam789@gmail.com>
2023-09-12 05:05:53 -0700

    Add the routes for the website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HeadphonesComponent } from './headphones/headphones.component';
+import { SpeakersComponent } from './speakers/speakers.component';
+import { EarphonesComponent } from './earphones/earphones.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: AppComponent},
+  { path: 'headphones', component: HeadphonesComponent },
+  { path: 'speakers', component: SpeakersComponent },
+  { path: 'earphones', component: EarphonesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeadphonesComponent } from './headphones/headphones.component';
+import { EarphonesComponent } from './earphones/earphones.component';
+import { SpeakersComponent } from './speakers/speakers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeadphonesComponent,
+    EarphonesComponent,
+    SpeakersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
